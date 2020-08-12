f4ae0c805ea6ff7fd1b41a0410a74008892bd69e
baschenbrenner <baschenbrenner@hotmail.com>
2020-08-12 17:43:05 -0500

    fixed specs

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,16 +1,6 @@
 
 describe "methods practice" do
 
-  it "wrote out the non-timing steps for the tooth brushing" do
-
-    expect($stdout).to receive(:puts).with("Get your toothbrush out")
-    expect($stdout).to receive(:puts).with("Put a pea sized amount of toothpaste on your toothbrush")
-    expect($stdout).to receive(:puts).with("You can choose to put water on your toothbrush and toothpaste, if you choose")
-    tooth_brush("90 seconds")
-
-  end
-
-
   it "can respond to different input times" do
     expect($stdout).to receive(:puts).with("Get your toothbrush out")
     expect($stdout).to receive(:puts).with("Put a pea sized amount of toothpaste on your toothbrush")
@@ -30,4 +20,9 @@ describe "methods practice" do
   it "returns something other than nil" do
     expect(tooth_brush("90 seconds")).to be_a_kind_of(String)
   end
+
+  it "returns a string that indicates cleaning has occurred" do
+    expect(tooth_brush("90 seconds")).to include("clean")
+  end
+
 end
